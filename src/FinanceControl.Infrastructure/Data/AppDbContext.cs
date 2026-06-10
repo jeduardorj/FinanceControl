ace856f0ddd718576b5431101902d70a4419ca1b
jeduardorj <zeduardo.aguiar@gmail.com>
2026-06-10 18:54:51 -0300

    chore: configure ef core, dbcontext and initial migration

---
+++ src/FinanceControl.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControl.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly
+        );
+    }
+}
---
+++ src/FinanceControl.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FinanceControl.Infrastructure.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),
+                "../FinanceControl.API"))
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseSqlServer(
+            configuration.GetConnectionString("DefaultConnection")
+        );
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
